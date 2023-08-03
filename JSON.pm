6282a25f3a0d391efed9c43f300ce4a69749bec9
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-08-04 01:10:32 +0200

    Prepare for 0.03 version

--- JSON.pm
+++ JSON.pm
@@ -38,7 +38,7 @@ Readonly::Hash our %REL => (
 Readonly::Scalar our $DR => 'dwgread';
 Readonly::Scalar our $DW => 'dwgwrite';
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -266,6 +266,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
